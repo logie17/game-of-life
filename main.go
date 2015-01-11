1ae21cb782549316c705294950368b6216b6ec9b
Logan Bell <loganbell@gmail.com>
2015-01-11 14:26:09 -0800

    fmt

--- main.go
+++ main.go
@@ -7,44 +7,44 @@ import (
 )
 
 const (
-	boardWidth = 79
+	boardWidth  = 79
 	boardHeight = 30
 )
 
 type Cell struct {
-	alive  bool
+	alive bool
 }
 
-func (c Cell) Alive() bool{
+func (c Cell) Alive() bool {
 	return c.alive
 }
 
 type Grid struct {
-	currentGrid [][]Cell
+	currentGrid  [][]Cell
 	previousGrid [][]Cell
 }
 
-func(g *Grid) SetCurrentGrid(grid [][]Cell){
+func (g *Grid) SetCurrentGrid(grid [][]Cell) {
 	g.currentGrid = grid
 }
 
-func(g *Grid) SetPreviousGrid(grid [][]Cell){
+func (g *Grid) SetPreviousGrid(grid [][]Cell) {
 	g.previousGrid = grid
 }
 
-func (g Grid) CurrentGrid() ([][]Cell) {
+func (g Grid) CurrentGrid() [][]Cell {
 	return g.currentGrid
 }
 
-func (g Grid) PreviousGrid() ([][]Cell) {
+func (g Grid) PreviousGrid() [][]Cell {
 	return g.previousGrid
 }
 
-func (g *Grid) init (){
+func (g *Grid) init() {
 	g.currentGrid = g.initGrid()
 }
 
-func (g *Grid) initGrid() ([][]Cell) {
+func (g *Grid) initGrid() [][]Cell {
 	var newLife = make([][]Cell, boardWidth)
 	for i := range newLife {
 		newLife[i] = make([]Cell, boardHeight)
@@ -52,26 +52,26 @@ func (g *Grid) initGrid() ([][]Cell) {
 	return newLife
 }
 
-func (g *Grid) findAliveNeighbors(y int, x int) (int){
+func (g *Grid) findAliveNeighbors(y int, x int) int {
 	board := g.CurrentGrid()
 	aliveNeighbors := 0
 
-	if y >= 1 && x >= 1 && board[y - 1][x - 1].Alive() {
+	if y >= 1 && x >= 1 && board[y-1][x-1].Alive() {
 		aliveNeighbors++
 	}
-	if y >= 1 && x <= boardHeight && board[y - 1][x + 1].Alive() {
+	if y >= 1 && x <= boardHeight && board[y-1][x+1].Alive() {
 		aliveNeighbors++
 	}
-	if y <= boardWidth && x >= 1 && board[y + 1][x - 1].Alive() {
+	if y <= boardWidth && x >= 1 && board[y+1][x-1].Alive() {
 		aliveNeighbors++
 	}
-	if y <= boardWidth && x <= boardHeight && board[y + 1][x + 1].Alive() {
+	if y <= boardWidth && x <= boardHeight && board[y+1][x+1].Alive() {
 		aliveNeighbors++
 	}
-	if y >= 1 && board[y - 1][x].Alive() {
+	if y >= 1 && board[y-1][x].Alive() {
 		aliveNeighbors++
 	}
-	if y <= boardWidth &&  board[y + 1][x].Alive() {
+	if y <= boardWidth && board[y+1][x].Alive() {
 		aliveNeighbors++
 	}
 	if x >= 1 && board[y][x-1].Alive() {
@@ -80,13 +80,13 @@ func (g *Grid) findAliveNeighbors(y int, x int) (int){
 	if x <= boardHeight && board[y][x+1].Alive() {
 		aliveNeighbors++
 	}
-	
+
 	return aliveNeighbors
 }
 
-func (g *Grid) genLife() ([][]Cell){
+func (g *Grid) genLife() [][]Cell {
 	newLife := g.initGrid()
-	for y:= 1; y < boardWidth; y++ {
+	for y := 1; y < boardWidth; y++ {
 		for x := 1; x < boardHeight; x++ {
 			currentY := y - 1
 			currentX := x - 1
@@ -113,9 +113,9 @@ func (g *Grid) drawLife() {
 	currentLife := g.CurrentGrid()
 	// Draws the box
 	termbox.SetCell(0, 0, 0x250C, termbox.ColorWhite, termbox.ColorBlack)
-	termbox.SetCell(boardWidth + 1, 0, 0x2510, termbox.ColorWhite, termbox.ColorBlack)
-	termbox.SetCell(0, boardHeight + 1, 0x2514, termbox.ColorWhite, termbox.ColorBlack)
-	termbox.SetCell(boardWidth + 1, boardHeight + 1, 0x2515, termbox.ColorWhite, termbox.ColorBlack)
+	termbox.SetCell(boardWidth+1, 0, 0x2510, termbox.ColorWhite, termbox.ColorBlack)
+	termbox.SetCell(0, boardHeight+1, 0x2514, termbox.ColorWhite, termbox.ColorBlack)
+	termbox.SetCell(boardWidth+1, boardHeight+1, 0x2515, termbox.ColorWhite, termbox.ColorBlack)
 
 	for i := 1; i < 80; i++ {
 		termbox.SetCell(i, 0, 0x2500, termbox.ColorWhite, termbox.ColorBlack)
@@ -126,16 +126,15 @@ func (g *Grid) drawLife() {
 		termbox.SetCell(80, i, 0x2502, termbox.ColorWhite, termbox.ColorBlack)
 	}
 
-	for y:= 0; y < boardWidth; y++ {
+	for y := 0; y < boardWidth; y++ {
 		for x := 0; x < 30; x++ {
 			currentCell := currentLife[y][x]
-			
-			if currentCell.Alive() {
-				termbox.SetCell(y + 1, x + 1, '*', termbox.ColorWhite, termbox.ColorBlack)
-			} else {
-				termbox.SetCell(y + 1, x + 1, ' ', termbox.ColorWhite, termbox.ColorBlack)
-			}
 
+			if currentCell.Alive() {
+				termbox.SetCell(y+1, x+1, '*', termbox.ColorWhite, termbox.ColorBlack)
+			} else {
+				termbox.SetCell(y+1, x+1, ' ', termbox.ColorWhite, termbox.ColorBlack)
+			}
 
 		}
 	}
@@ -143,7 +142,7 @@ func (g *Grid) drawLife() {
 }
 
 func (g *Grid) seedLife() {
-	for i := 0; i < ( boardWidth * boardHeight / 2); i++ {
+	for i := 0; i < (boardWidth * boardHeight / 2); i++ {
 		g.currentGrid[rand.Intn(boardWidth)][rand.Intn(boardHeight)] = Cell{true}
 	}
 }
